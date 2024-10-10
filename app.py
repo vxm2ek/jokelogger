8723cdb47483539f48ecef8546c628d69a4b93cf
Eva Winston <vxm2ek@virginia.edu>
2024-10-10 14:14:40 +0000

    adding things

---
+++ app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import requests
+import json
+import logging
+
+url = "https://official-joke-api.appspot.com/random_joke"
+
+response = requests.get(url)
+response = json.loads(response.text)
+
+id = response['id']
+setup = response['setup']
+punchline = response['punchline']
+
+print(id, setup, punchline)
